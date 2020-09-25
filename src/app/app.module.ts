11e063a1173f2faee790e62e1320c7242da346bc
rakesh <kallurirakesh15@gmail.com>
2020-09-25 15:21:57 -0400

    Adding validations to the form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { AppComponent } from './app.component';
@@ -19,7 +20,9 @@ const appRoutes: Routes = [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
     FormsModule,
-    BsDatepickerModule,
+    BrowserAnimationsModule,
+    BsDatepickerModule.forRoot(),
+    BsDatepickerModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,6 +1,7 @@
 import { Department } from './../models/department.model';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-create-employee',
@@ -8,18 +9,32 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./create-employee.component.css'],
 })
 export class CreateEmployeeComponent implements OnInit {
+  datePickerConfig: Partial<BsDatepickerConfig>;
   gender = 'male';
   isActive = true;
+  photoPath = '';
+  previewPhoto = false;
   departments: Department[] = [
     { id: 1, name: 'Help Desk' },
     { id: 2, name: 'HR' },
     { id: 3, name: 'IT' },
     { id: 4, name: 'Payroll' },
   ];
-  constructor() {}
+  constructor() {
+    this.datePickerConfig = Object.assign(
+      {},
+      {
+        containerClass: 'theme-dark-blue',
+        dateInputFormat: 'DD/MM/YYYY',
+      }
+    );
+  }
 
   ngOnInit(): void {}
   saveEmployee(employeeForm: NgForm): void {
     console.log(employeeForm);
+  }
+  togglePhotoPreview(): void {
+    this.previewPhoto = !this.previewPhoto;
   }
 }
