6295b83386fd63ca3986a478363fab3ed5001b21
rakesh <kallurirakesh15@gmail.com>
2020-09-25 11:22:12 -0400

    added form template

---
+++ src/app/employees/create-employee.component.ts
@@ -0,0 +1,25 @@
+import { Department } from './../models/department.model';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css'],
+})
+export class CreateEmployeeComponent implements OnInit {
+  gender = 'male';
+  isActive = true;
+  departments: Department[] = [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'Payroll' },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+  saveEmployee(employeeForm: NgForm): void {
+    console.log(employeeForm);
+  }
+}
